fdf0acb03fa7179acc595e8c11909ab64c7d5301
ujjwal1808 <88647832+ujjwal1808@users.noreply.github.com>
2024-10-06 20:27:36 +0530

    config_commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/2048-in-react",
+    output: "export",
+    reactStrictMode: true,
+  };
+  
 
 export default nextConfig;
